693c560337f50e75eda4fbc49e0bd91b1264b3ae
Howie Huntington <robert.huntington1@t-mobile.com>
2022-05-23 14:19:19 -0400

    tic tac toe progress

--- app.js
+++ app.js
@@ -1,56 +1,110 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 
-
-function main() {
-    var dataSet = ["aaa", "aab", "aac", "aba", "aca", "baa", "caa"];
-    var dictionary = new Dictionary(dataSet);
-    query('a', 5);
+function play() {
+    console.log('hello idiot');
+    var playerX = new Player('X', true);
+    var playerO = new Player('O', false);
+    var game = new Game(playerX, playerO);
+    console.log(game.viewBoard());
 }
 
-function query(userInput, limit) {
-    var dataSet = ["aaa", "aab", "aac", "aba", "aca", "baa", "caa"];
-    // dataSet.filter(value => {
-    //     set flag
-    // });
-    var returnedArray = [];
-    var stringNumberValues = {
-        "aaa": 0, 
-        "aab": 0, 
-        "aac": 0, 
-        "aba": 0, 
-        "aca": 0, 
-        "baa": 0, 
-        "caa": 0
+
+
+
+class Game {
+    moves;
+    board;
+    playerA;
+    playerB;
+    winningMoves;
+    winner;
+
+    constructor(playerX, playerO) {
+        this.moves = [];
+        this.board = [
+            ['_', '_', '_'],
+            ['_', '_', '_'],
+            ['_', '_', '_']
+        ];
+        this.playerX = playerX;
+        this.playerO = playerO;
+        this.winningMoves = [
+            ['00', '01', '02'],
+            ['10', '11', '12'],
+            ['20', '21', '22'],
+            ['00', '10', '20'],
+            ['01', '11', '21'],
+            ['02', '12', '22'],
+            ['00', '11', '22'],
+            ['02', '11', '20'],
+        ];
+        winner = '';
     }
 
-    for(let i = 0; i < dataSet.length; i++ ) {
-        let x = 0;
-        for(let k = 0; k < userInput.length && dataSet[i].length; k++) {
-            if (userInput[k] === dataSet[i].charAt(k)) {
-                x++;
+    checkForWin(player) {
+        for (let move in this.winningMoves) {
+            if (player.moves.includes(move)) {
+                return player.getName();
             }
-            // console.log(dataSet[i]);
-            // console.log(dataSet[i].indexOf(k));
         }
-        stringNumberValues[dataSet[i]] = x;
+    }
+
+    addMove(x, y, player) {
+        if (this.board[x][y].valueOf === '_') {
+            this.board[x][y] = player.name;
+            player.moves.push(x + '' + y);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    viewBoard() {
+        var view = '';
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                view += this.board[x][y] + ' ';
+                if (y === 2) {
+                    view += '\n';
+                }
+            }
+        }
+        return view;
     }
 }
 
-class Dictionary {
-    dictionary;
-    constructor(dictionary) {
-        this.dictionary = dictionary;
+class Player {
+    name;
+    moves;
+    isTurn;
+
+    constructor(name, isTurn) {
+        this.name = name;
+        this.isTurn = isTurn;
+        this.moves = [];
     }
 
-    setDictionary(dictionary) {
-        this.dictionary = dictionary;
+    getName() {
+        return this.name;
     }
 
-    getDictionary() {
-        return this.dictionary;
+    setName(name) {
+        this.name = name
+    }
+
+    addMove(params) {
+        this.moves.push(params);
     }
 }
 
-main();
 
-module.exports = {Dictionary};
+play();
+
+
+
+module.exports = {Game, Player};
--- app.test.js
+++ app.test.js
@@ -1,19 +1,11 @@
 var app = require('./app.js');
 
 
-// test('Test for hello world', () => {
-//     var testData = 'HELLO WORLD';
-//     expect(app.getString('Hello World')).toBe(testData);
-// });
+test('Test for hello world', () => {
+    // var testData = 'HELLO WORLD';
+    // expect(app.getString('Hello World')).toBe(testData);
+    var dog = new app.Dog('woof');
+    var test = 'woof';
+    expect(dog.bark()).toBe(test);
 
-test('Test to verify that data set was ingested correctly', () => {
-    var testData = ["aaa", "aab", "aac", "aba", "aca", "baa", "caa"];
-    var dictionary = new app.Dictionary(testData);
-    expect(dictionary.getDictionary()).toEqual(["aaa", "aab", "aac", "aba", "aca", "baa", "caa"]);
-});
-
-test('Check to make sure no false positive', () => {
-    var testData = ["aba", "aab", "aac", "aba", "aca", "baa", "caa"];
-    var dictionary = new app.Dictionary(testData);
-    expect(dictionary.getDictionary()).not.toEqual(["aaa", "aab", "aac", "aba", "aca", "baa", "caa"]);
 });
